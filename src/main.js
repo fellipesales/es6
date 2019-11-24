909da2f34bc2b0336ffb8fcd9e09f5f072fe6207
Fellipe Sales <fsales@rcasistemas.com.br>
2019-11-24 19:55:38 -0300

    first

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+const minhaPromisse = () => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('OK')
+    }, 2000);
+});
+
+async function execPromisse(){
+    const response = await minhaPromisse();
+
+    console.log(response);
+}
+
+execPromisse();
