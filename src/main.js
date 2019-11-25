2e742292b826fba5ecbf59eb2d00ccccbfe1d924
Fellipe Sales <fsales@rcasistemas.com.br>
2019-11-25 09:00:09 -0300

    correção erro, espaço no arquivo babel.rc

--- src/main.js
+++ src/main.js
@@ -13,3 +13,28 @@ async function execPromisse(){
 
 execPromisse();
 
+
+
+function base(x){
+    return function produto(y){
+        return x * y;
+    }
+}
+
+var f = base(2);
+var g = base(-1);
+
+
+
+function funX(n) {
+    if (n == 1) {
+        return (n);
+    }
+    return ((n - 1) * funX(n - 1));
+}
+
+
+
+console.log(funX(6));
+
+console.log(f(3) + g(-1));
